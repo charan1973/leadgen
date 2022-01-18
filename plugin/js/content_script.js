93626c3c21b1e65440147b4fd94f8024caba2e8c
charan1973 <v.vicky1973@gmail.com>
2022-01-18 22:38:16 +0530

    update: completed plugin implementation

--- plugin/js/content_script.js
+++ plugin/js/content_script.js
@@ -1,12 +1,31 @@
-window.addEventListener("load", (e) => {
-    const companyName = document.querySelector("[aria-label='Current company']").innerText;
-    let fullName = document.querySelector(".global-nav__me-photo").alt;
-    fullName = fullName.split(" ");
+window.addEventListener("load", async function load(e) {
+    window.removeEventListener("load", load, false);
+
+    let fullName = document.querySelector(".artdeco-entity-lockup__title");
+    fullName = fullName.innerText;
+    fullName = fullName && fullName.split(" ");
+
+    let companyName = document.querySelector("[aria-label='Current company']");
+    companyName = companyName !== null ? companyName.innerText : null;
+
     const leadData = {
-        companyName,
-        firstName: fullName[0],
-        lastName: fullName[1]
+        company_name: companyName,
+        first_name: fullName[0],
+        last_name: fullName[1]
     };
 
-    console.log(leadData);
+    try {
+        const res = await fetch("http://localhost:3000/api/v1/leads", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+                'Accept': '*/*'
+            },
+            body: JSON.stringify(leadData)
+        }).then(r => r.json());
+        console.log(res);
+    } catch (error) {
+        console.log(error);
+    }
+
 });
